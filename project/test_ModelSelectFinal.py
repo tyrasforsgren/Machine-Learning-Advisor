34532f7508dfc122c31dae24e19cdecaf80dea02
Tyra Forsgren <forsgren.tyra@gmail.com>
2023-09-19 10:06:02 +0200

    test_load_data_success working

--- project/test_ModelSelectFinal.py
+++ project/test_ModelSelectFinal.py
@@ -10,32 +10,7 @@ from ModelSelectFinal import ModelSelection
 
 
 class TestModelSelection(unittest.TestCase):
-    '''
 
-    def setUp(self):
-        self.test_data = pd.DataFrame({
-            'ind':[1,2,3,5,6,7],
-            'ind2':[3,4,5,6,7,8],
-            'dep':[4,5,6,8,9,2]
-            })
-        
-        # mode='w+' mwan writing+reading mode simlutaneously
-        # delete=True (default) means file will be deleted upon exit with-statement
-        self.temp_file = NamedTemporaryFile(
-            dir=os.path.abspath(r'C:\Users\tyraf\Documents\term_3_python_ai\repos\testing_prev_ml_exam\project'),
-            mode='w+',
-            suffix='.csv',
-            delete=False)
-        
-        self.test_data.to_csv(self.temp_file, index=False)
-        self.temp_file.close()
-
-    def tearDown(self):
-        
-        # self.temp_file.close()
-        os.remove(self.temp_file.name)
-        #pass
-    '''
 
     def setUp(self) -> None:
         # Test with clean data
@@ -51,36 +26,25 @@ class TestModelSelection(unittest.TestCase):
 
     def test_load_data_success(self) -> None:
         test_instance = ModelSelection()
-        with patch ''
+        with patch ('pandas.read_csv') as mock_read_csv:
+            mock_read_csv.return_value = self.basic_clean_data
+
+            with patch ('ModelSelectFinal.ModelSelection.set_values') as mock_set_values:
+                mock_X = self.basic_clean_data.drop('dep',axis=1)
+                mock_y = self.basic_clean_data['dep']
+                mock_set_values.return_value = mock_X, mock_y
+
+                test_instance.load_data('path doesnt mater due to mock_read_csv')
+                mock_set_values.assert_called_once()
+
+            self.assertIsNotNone(test_instance.data)
+            pd.testing.assert_frame_equal(test_instance.data, self.basic_clean_data)  # Compare DataFrames
+            pd.testing.assert_frame_equal(test_instance.X, mock_X)  # Compare Series
+            pd.testing.assert_series_equal(test_instance.y, mock_y)  # Compare Series
+
 
     def test_set_values(self):
-        model_selection = ModelSelection()
-        model_selection.data = self.basic_clean_data
-        with patch('builtins.input', return_value='dep'):
-            # mock the user choosing the 'dep' column as dependant value
-            pass
-            
-    
-        # Assert that the created x column is the same
-        self.assertEqual(model_selection.X.columns.tolist(),
-                         self.test_data.drop('dep',axis=1).columns.tolist())
-        # Assert that the Dependant label name is the same
-        self.assertEqual(model_selection.y.name, 'dep')
-
-    def test_complete_data_perfect(self) -> None:
         pass
-
-    def test_complete_data_remove_missing(self) -> None:
-        with NamedTemporaryFile(dir=r'C:\Users\tyraf\Documents\term_3_python_ai\repos\testing_prev_ml_exam\project',
-                            mode='w+',
-                            suffix='.csv',
-                            delete=False) as missing_temp_file:
-        
-            missing_file_data = pd.DataFrame({'ind':[1,None,5,7],'dep':[1,2,3,4]}) # Create missing value
-            missing_file_data.to_csv(missing_temp_file, index=False)
-
-            with patch('builtins.input', return_value='Y'):
-                model_selection = ModelSelection(missing_temp_file.name)
 
 
     def test_regressor_selection(self):
